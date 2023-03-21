7822f363a08a5562784b7385a8e6c2335e061659
NgoMinhToan <mtoan2610@gmail.com>
2023-03-21 10:30:34 +0700

    update

--- pwd.sh
+++ pwd.sh
@@ -1,14 +1,22 @@
+echo ""
+echo "================================================================"
 echo "Init Submodules"
 git submodule init
 
+echo ""
+echo "================================================================"
 echo "Update Submodules"
 git submodule update
 
+echo ""
 echo "================================================================"
 echo "Run docker compose"
 docker compose -f docker-compose.playwithdocker.yml up -d
 
+echo ""
+echo "================================================================"
 echo "Init news"
 docker exec vnexpress_clone-server-1 python manage.py auto_update_news big
 
-echo "Finished"
+echo ""
+echo "Finished!"
